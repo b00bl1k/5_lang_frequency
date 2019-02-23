f0bab9c6f7c17592a1268483b4a6dc54c52e9270
Alexey Ryabov <6006l1k@gmail.com>
2019-02-23 21:06:14 +0300

    Solve task

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+The script takes as an input the path to the file and considers the frequency of words in the text.
+
+# Quickstart
+
+Example of script launch on Linux, Python 3.5:
+
+```python
+
+$ python lang_frequency.py text.txt
+Most frequent words in text:
+ в:6
+ и:5
+ на:3
+ скрипт:3
+ не:2
+```
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,42 @@
+import string
+import sys
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, "r", encoding="utf8") as fh:
+        return fh.read()
+
+
+def split_text_by_words(text):
+    table = str.maketrans(dict.fromkeys(string.punctuation))
+    without_punctuation = text.translate(table)
+    return without_punctuation.lower().split()
 
 
 def get_most_frequent_words(text):
-    pass
+    words = split_text_by_words(text)
+    freq_dict = {}
+    for word in words:
+        if word in freq_dict.keys():
+            freq_dict[word] += 1
+        else:
+            freq_dict[word] = 1
+    top_words = sorted(freq_dict.items(), key=lambda kv: kv[1], reverse=True)
+    return top_words[:5]
 
 
-if __name__ == '__main__':
-    pass
+def main():
+    try:
+        result = load_data(sys.argv[1])
+        words = get_most_frequent_words(result)
+        print("Most frequent words in text:")
+        for word in words:
+            print(" {}:{}".format(*word))
+    except FileNotFoundError:
+        sys.exit("File not found")
+    except IndexError:
+        sys.exit("Invalid command line arguments")
+
+
+if __name__ == "__main__":
+    main()
